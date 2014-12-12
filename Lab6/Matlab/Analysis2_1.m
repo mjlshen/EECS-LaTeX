b0c93df58d6b690adc482d1e707488a9d9a4d96e
Michael Shen <mjlshen@yahoo.com>
2014-12-11 19:51:53 -0500

    Lab 6 Writeup

--- Lab6/Matlab/Analysis2_1.m
+++ Lab6/Matlab/Analysis2_1.m
@@ -1,5 +1,5 @@
-%EECS 230 Lab 1
-%Analysis 2.3 Plots
+%EECS 230 Lab 6
+%Analysis 2.1 Plots
 close
 clear
 
@@ -7,7 +7,20 @@ RL = 14.83;
 w = 46.973e6;
 M = [822 714 492 364 278 208 174 152]*10^(-9);
 R = 0.47;
+dist = [2 4 6 8 10 12 14 16];
 
 eta_num = 4*RL^2*w^2.*M.^2;
 eta_den = ((R+RL)^2+w^2.*M.^2).^2;
-eta = eta_num./eta_den
+eta = eta_num./eta_den;
+
+eta2 = RL^2/(R + RL)^2;
+
+etamax_num = 4*(R^2 + w^2.*M.^2)*w^2.*M.^2;
+etamax_den = ((R + sqrt(R^2 + (w.*M).^2)).^2 + (w.*M).^2).^2;
+etamax = etamax_num./etamax_den;
+
+plot(dist, etamax)
+title('Maximum Power Transfer Efficiency of Shielded Loop Resonators')
+xlabel('Coupling Distance (cm)')
+ylabel('Maximum Power Transfer Efficiency')
+axis([0 16 0.8 1])
---
+++ Lab6/Matlab/Questions2_2.m
@@ -0,0 +1,7 @@
+%EECS 230 Lab 6
+%Questions 2.2 Plots
+close
+clear
+
+SdB = [-7.767 -4.622 -2.630 -1.176 -0.593 -0.938 -1.904 -3.360];
+Sconst = -20*log10(SdB)
